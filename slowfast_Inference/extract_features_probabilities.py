db3b6cf1bae7f77a673ec2c3305740415f47c66d
Tahakom-TDAL <130108825+Tahakom-TDAL@users.noreply.github.com>
2023-04-13 12:10:27 +0300

    Update extract_features_probabilities.py

--- slowfast_Inference/extract_features_probabilities.py
+++ slowfast_Inference/extract_features_probabilities.py
@@ -12,7 +12,6 @@ import subprocess
 config_path = '/mnt/lustre/talhussan/TDAL/slowfast_Inference/configs/SLOWFAST_8x8_R50.yaml'
 checkpoint_path = '/mnt/lustre/talhussan/TDAL/slowfast_Inference/checkpoints/checkpoint_epoch_01010_rear.pyth'
 checkpoint_type = 'pytorch'
-model_name = 'slowfast1_epoch_1010'
 angle = 'Rear'
 videos_segments = glob.glob(f'/mnt/lustre/TAHAKOM/ActionRecognition/A2_v2_segmentation/t1/{angle}/*')
 Out = '' 
@@ -20,11 +19,21 @@ Out = ''
 def main (config_path, checkpoint_path, checkpoint_type, model_name, angle, videos_segments, Out) : 
 
     videos_segments =  glob.glob(videos_segments+'/*')
+    
+    
+    
+    if angle == 3: 
+        angle_name = 'Rear'
+    elif angle == 2:
+        angle_name = 'Right'
+    else:
+        angle_name = ''
+
 
     for video_segments in videos_segments:
 
         data_path = video_segments
-        saved_path_for_features_and_probability = Out + '/' + angle+'_features_and_probability_t1/' + model_name + '/' + data_path.split("/")[-1]
+        saved_path_for_features_and_probability = Out + '/' + angle+'_features_and_probability_t1/' + data_path.split("/")[-1]
         os.makedirs(saved_path_for_features_and_probability, exist_ok = True)
 
         print("========saved_path_for_features_and_probability========")
@@ -50,16 +59,13 @@ if __name__ == "__main__":
     parser.add_argument('--checkpoint_path', metavar='path',
                         help='the path to the checkpoint.. ')
     
-    parser.add_argument('--model_name', metavar='name', required=False,
-                        help='model name .. ')
+    parser.add_argument('--angle', metavar='view type',
+                        help='view type 2 for Right and 3 for Rear ..')
     
-    parser.add_argument('--angle', metavar='view type', required=False,
-                        help='view type Rear or Right ..')
-    
-    parser.add_argument('--videos_segments', metavar='path', required=False,
+    parser.add_argument('--videos_segments', metavar='path',
                         help='the path of parent where the segments folders will be saved..')
     
-    parser.add_argument('--dist_path', metavar='path', required=False,
+    parser.add_argument('--dist_path', metavar='path', 
                         help='view type Rear or Right ..')
 
     args = parser.parse_args()
